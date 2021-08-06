42e019687d1086353f5776784849d42499504f85
igor gorchakov <gorchakov.igiv@gmail.com>
2021-08-06 13:15:29 +0300

    fix

--- src/main/java/edu/cp/algebra/fundamentals/BinaryExponentiation.java
+++ src/main/java/edu/cp/algebra/fundamentals/BinaryExponentiation.java
@@ -23,6 +23,7 @@ public class BinaryExponentiation {
     //time - O(log b), space - O(1)
     public long binPowMod(long a, long b, long m){
         long res = 1;
+        a %= m;
         while (b > 0) {
             if ((b & 1) == 1)
                 res = (res * a) % m;
